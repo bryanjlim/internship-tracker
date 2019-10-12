c40076a0c96142ddf4190e4f4e435bcef68443f4
Spencer Lu <spenlu@uw.edu>
2019-10-12 15:49:41 -0700

    Add status, updates to Application

--- src/dataCollections.js
+++ src/dataCollections.js
@@ -1,9 +1,24 @@
-let whitelist = ["microsoft", "bloomberg", "AI2", "facebook", "google", "flatiron"];
+const whitelist = ["microsoft", "bloomberg", "AI2", "facebook", "google", "flatiron"];
 
+const companyList = [];
+
+const applyStatus = {
+    PROCESSING: "processing",
+    INTERVIEWING: "interviewing",
+    PROCESSING: "processing",
+    ACCEPTED: "accepted",
+    DENIED: "denied"
+}
+
+// TODO replace lastUpdate with time
 class Application {
+    emails;
+
     constructor(company){
         this.company = company;
-        this.appliedDay = 0;
+        this.lastUpdate = 0;
+        this.status = applyStatus.PROCESSING;
+        this.emails = [];
     }
 
     updateStatus(status){
@@ -14,20 +29,30 @@ class Application {
         return this.company;
     }
 
-    getTimeElapsed() {
-        // TODO replace with time
-        return 0 - this.appliedDay;
+    getTimeLastUpdate() {
+        return 0 - this.lastUpdate;
     }
 
     getStatus() {
         return this.status;
     }
+
+    addEmail(email) {
+        this.emails.push(email);
+        this.lastUpdate = 0;
+    }
+
+    getListEmails() {
+        return this.emails;
+    }
+
 }
 
 class Company {
     constructor(name, email) {
         this.name = name;
         this.email = email;
+        companyList.push(this);
     }
 
     getName() {
@@ -36,5 +61,9 @@ class Company {
 
     getEmail() {
         return this.email;
+    }
+
+    getAvgTime() {
+        return "two weeks";
     }
 }
