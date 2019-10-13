137698a281fec74d69b3e34916ed314031790a0e
bryanjlim <bryanlim@cs.washington.edu>
2019-10-13 00:47:59 -0700

    Fix firebase auth

--- src/App.js
+++ src/App.js
@@ -1,76 +1,51 @@
 import React, { Component } from "react";
 import firebase from "firebase";
 import "./App.css";
-import { Application } from "./dataCollections"; 
+import { Application } from "./dataCollections";
 
 export class App extends Component {
   constructor(props) {
     super(props);
 
-    const config = {
-      apiKey: "AIzaSyDoj6K6NL3JzKbUv-K424ybISws9d16UhY",
-      authDomain: "track-my-internship.firebaseapp.com",
-      databaseURL: "https://track-my-internship.firebaseio.com",
-      projectId: "track-my-internship",
-      storageBucket: "track-my-internship.appspot.com",
-      messagingSenderId: "700705257439",
-      appId: "1:700705257439:web:7bceaee157b0e60d80366a",
-      measurementId: "G-5QG71NE3VY"
+    const firebaseConfig = {
+      apiKey: "AIzaSyD6jrZUJhMJamgvhO1-FkKQA9rh-nbyr2o",
+      authDomain: "track-internship.firebaseapp.com",
+      databaseURL: "https://track-internship.firebaseio.com",
+      projectId: "track-internship",
+      storageBucket: "track-internship.appspot.com",
+      messagingSenderId: "766437384638",
+      appId: "1:766437384638:web:7287037185806f5bd699ef"
     };
-
-    if (!firebase.apps.length) {
-      firebase.initializeApp(config);
-    }
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
 
     this.state = {
       isSignedIn: false
     };
 
-    firebase.auth().onAuthStateChanged(user => {
-      if (user) {
-        this.setState({isSignedIn: true})
-        const db = firebase.database();
-        let years = [];
-        let userData = {
-          mostRecentTime: "04/01/2004"
-        };
-        db.ref("/" + user.uid)
-          .once("value")
-          .then(value => {
-            const items = value.toJSON();
-            if (items != null) {
-              years = items[years];
-              userData = items[userData];
-            }
-          });
-        this.fetchData(userData.mostRecentTime);
-      } else {
-        console.log("ERR: No User");
-      }
-    });
-
     this.signIn = this.signIn.bind(this);
     this.googleAuthentication = this.googleAuthentication.bind(this);
   }
 
-  fetchData(mostRecentTime) {
-    console.log("fetching data")
-    var url = new URL('http://localhost:9000/getData');
-    let authToken = firebase.auth().currentUser.getIdToken(true)
-      .then(function (token) {
-      let userId = firebase.auth().currentUser.uid;
-      let params = {"authToken": token, 
-                    "userId" : userId,
-                    "mostRecentTime": mostRecentTime};
-      url.search = new URLSearchParams(params);
-      fetch(url).then(function(response) {
+  fetchData(mostRecentTime, authToken) {
+    console.log("fetching data");
+    var url = new URL("http://localhost:9000/getData");
+    let userId = firebase.auth().currentUser.uid;
+    let params = {
+      authToken,
+      userId,
+      mostRecentTime
+    };
+    url.search = new URLSearchParams(params);
+    fetch(url)
+      .then(function(response) {
         // The response is a Response instance.
         // You parse the data into a useable format using `.json()`
         return response.json();
-      }).then(function(data) {
+      })
+      .then(function(data) {
         console.log("returned something");
       });
-    });
   }
 
   render() {
@@ -87,7 +62,22 @@ export class App extends Component {
 
   signIn() {
     this.googleAuthentication()
-      .then(() => {
+      .then((user, authToken) => {
+        const db = firebase.database();
+        let years = [];
+        let userData = {
+          mostRecentTime: "04/01/2004"
+        };
+        db.ref("/" + user.uid)
+          .once("value")
+          .then(value => {
+            const items = value.toJSON();
+            if (items != null) {
+              years = items[years];
+              userData = items[userData];
+            }
+          });
+        this.fetchData(userData.mostRecentTime, authToken);
         this.setState({ isSignedIn: true });
       })
       .catch(e => {
@@ -103,10 +93,7 @@ export class App extends Component {
         .auth()
         .signInWithPopup(provider)
         .then(function(result) {
-          console.log('asdfsd');
-          // This gives you a Google Access Token. You can use it to access the Google API.
-          // The signed-in user info.
-          res();
+          res(result.user, result.credential.accessToken);
         })
         .catch(function(error) {
           // Handle Errors here.
